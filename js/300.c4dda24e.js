c195fb1dc02484bebd2c658c38156f73b5cfe758
faster-area <developper.chausson@gmail.com>
2022-10-10 18:44:02 +0000

    Update from https://github.com/EpitechPromo2025/B-DEV-500-LIL-5-1-area-clement.de-laage-de-meux/commit/48c4acc571d56276060e8001d162eb7703bf3632

---
+++ js/300.c4dda24e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkArea"]=self["webpackChunkArea"]||[]).push([[300],{6300:function(e,s,u){u.r(s);const a={},c=a;s["default"]=c}}]);
+//# sourceMappingURL=300.c4dda24e.js.map
